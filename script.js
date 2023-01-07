895962f0c0ac09b8a5f3ab1d0b7f4ad4d22a2daf
imReset <fffffreinds101@gmail.com>
2023-01-06 20:56:41 -0800

    setup  + add, subtract, multiple, and divide

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello Reeva!");
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
